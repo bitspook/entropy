d71d5725b82aa23c25c459868bbd72f1aed763f0
Charanjit Singh <ckhabra@gmail.com>
2021-07-16 21:47:50 +0530

    feat: host static assets via rocket
    With this I think we are ready to now fetch the events from database and put
    them in this template.
    
    Some time soon I'll add scss compilation and other development utilities in this
    server. Gonna be fun I think.

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,19 +1,25 @@
-use rocket::{
-    figment::{providers::Env, Figment, Profile},
-    Build, Rocket,
-};
+use std::collections::HashMap;
+
+use rocket::{Build, Rocket, figment::{providers::Env, Figment, Profile}, fs::FileServer};
+use rocket_dyn_templates::Template;
 
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello world"
+fn index() -> Template {
+    let context: HashMap<String, String> = HashMap::new();
+
+    Template::render("index", context)
 }
 
 fn app() -> Rocket<Build> {
     let figment = Figment::from(rocket::Config::default())
+        .merge(("template_dir", "src/web/templates"))
         .merge(Env::prefixed("ENTROPY_").global())
         .select(Profile::from_env_or("ENTROPY_PROFILE", "default"));
 
-    rocket::custom(figment).mount("/", routes![hello])
+    rocket::custom(figment)
+        .mount("/", routes![index])
+        .mount("/", FileServer::from("src/web/static"))
+        .attach(Template::fairing())
 }
 
 pub async fn run() -> () {
