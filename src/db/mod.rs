9a3be0e3b790a3f0cb148b5563268126abecf4e6
Charanjit Singh <bitspook@protonmail.com>
2021-11-02 19:08:20 +0530

    chore: gh-actions creds for remote postgres/minio
    Explicitly state environment

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -8,7 +8,6 @@ pub mod schema;
 pub fn establish_connection() -> anyhow::Result<PgConnection> {
     let config = crate::EntropyConfig::load()?;
     let database_url = config.database_url;
-
     let conn = PgConnection::establish(&database_url).map_err(Error::from)?;
 
     Ok(conn)
